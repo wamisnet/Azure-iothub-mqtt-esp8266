e613f344f6ed960c1888ca07dc652033595c225e
FS-ichikawa <riku.ichikawa@gmail.com>
2021-09-07 10:53:51 +0900

    Code formatting

--- examples/Azure-iothub-mqtt-esp8266/Azure-iothub-mqtt-esp.ino
+++ examples/Azure-iothub-mqtt-esp8266/Azure-iothub-mqtt-esp.ino
@@ -1,31 +1,34 @@
 #include <EspAzureIoTHub.h>
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   WiFi.begin("SSID", "PASS");
   Azure.begin("Your Key"); //YourKey Example:"HostName=YourHost.azure-devices.net;DeviceId=YourDevice;SharedAccessKey="
   Azure.setCallback(azureCallback);
 }
 
-void azureCallback(String s) {
+void azureCallback(String s)
+{
   Serial.print("azure Message arrived [");
   Serial.print(s);
   Serial.println("] ");
 }
 
-void loop() {
-  if (WiFi.status() == WL_CONNECTED) {
+void loop()
+{
+  if (WiFi.status() == WL_CONNECTED)
+  {
     Azure.connect();
     DataElement a = DataElement();
     a.setValue("EspAnalog", analogRead(A0));
     Azure.push(&a);
     Serial.println("pushed");
     delay(2000);
-  } else {
+  }
+  else
+  {
     Serial.println("Not connected to the Internet");
     delay(250);
   }
 }
-
-
-
